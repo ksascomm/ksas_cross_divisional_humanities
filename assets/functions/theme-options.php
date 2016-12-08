d0d28ab485e7a9b702570c659df82668829e1bf3
ksascomm <ksasweb@jhu.edu>
2016-12-08 13:27:02 -0500

    clear transients when posts updated

--- assets/functions/theme-options.php
+++ assets/functions/theme-options.php
@@ -6,7 +6,7 @@
  * @return array
  */
 function flagship_sub_options_page_sections() {
-	
+
 	$sections = array();
 	// $sections[$id] 				= __($title, 'flagship_sub_textdomain');
 	$sections['homepage_section'] 	= __('Homepage Options', 'flagship_sub_textdomain');
@@ -14,11 +14,11 @@ function flagship_sub_options_page_sections() {
 	$sections['footer_section'] 	= __('Footer Options', 'flagship_sub_textdomain');
 	$sections['technical_section'] 	= __('Technical Options', 'flagship_sub_textdomain');
 	$sections['directory_section']  = __('Directory Search Options', 'flagship_sub_textdomain');
-	return $sections;	
+	return $sections;
 }
 
 /**
- * Define our form fields (settings) 
+ * Define our form fields (settings)
  *
  * @return array
  */
@@ -26,7 +26,7 @@ function flagship_sub_options_page_fields() {
 	// Text Form Fields section
 	// Select Form Fields section
 	$options[0] =
-	array (		
+	array (
 		"section" => "homepage_section",
 		"id"      => FLAGSHIP_SUB_SHORTNAME . "_feed_name",
 		"title"   => __( 'Homepage Sub-head', 'flagship_sub_textdomain' ),
@@ -35,7 +35,7 @@ function flagship_sub_options_page_fields() {
 		"class"   => "nohtml",
 		"std"    => "");
 	$options[1] =
-	array (		
+	array (
 		"section" => "homepage_section",
 		"id"      => FLAGSHIP_SUB_SHORTNAME . "_news_quantity",
 		"title"   => __( 'Homepage Posts', 'flagship_sub_textdomain' ),
@@ -44,7 +44,7 @@ function flagship_sub_options_page_fields() {
 		"class"   => "numeric",
 		"std"    => "");
 	$options[2] =
-	array (		
+	array (
 		"section" => "homepage_section",
 		"id"      => FLAGSHIP_SUB_SHORTNAME . "_slider_style",
 		"title"   => __( 'Homepage slider style', 'flagship_sub_textdomain' ),
@@ -53,7 +53,7 @@ function flagship_sub_options_page_fields() {
 		"choices" => array("horizontal", "vertical"),
 		"std"    => "vertical");
 	$options[3] =
-	array (		
+	array (
 		"section" => "select_section",
 		"id"      => FLAGSHIP_SUB_SHORTNAME . "_breadcrumbs",
 		"title"   => __( 'Breadcrumbs', 'flagship_sub_textdomain' ),
@@ -61,15 +61,15 @@ function flagship_sub_options_page_fields() {
 		"type"    => "checkbox",
 		"std"    => "1");
 	$options[4] =
-	array (		
+	array (
 		"section" => "directory_section",
 		"id"      => FLAGSHIP_SUB_SHORTNAME . "_directory_search",
 		"title"   => __( 'Directory Search', 'flagship_sub_textdomain' ),
 		"desc"    => __( 'Do you want a search box for your people directory?', 'flagship_sub_textdomain' ),
 		"type"    => "checkbox",
-		"std"    => "1");	
+		"std"    => "1");
 	$options[5] =
-	array (		
+	array (
 		"section" => "select_section",
 		"id"      => FLAGSHIP_SUB_SHORTNAME . "_breadcrumb_home",
 		"title"   => __( 'Breadcrumb Name', 'flagship_sub_textdomain' ),
@@ -78,7 +78,7 @@ function flagship_sub_options_page_fields() {
 		"class"   => "nohtml",
 		"std"    => "Home");
 	$options[6] =
-	array (		
+	array (
 		"section" => "technical_section",
 		"id"      => FLAGSHIP_SUB_SHORTNAME . "_google_analytics",
 		"title"   => __( 'Google Analytics ID', 'flagship_sub_textdomain' ),
@@ -87,7 +87,7 @@ function flagship_sub_options_page_fields() {
 		"class"   => "nohtml",
 		"std"    => "UA-40512757-1");
 	$options[7] =
-	array (		
+	array (
 		"section" => "technical_section",
 		"id"      => FLAGSHIP_SUB_SHORTNAME . "_search_collection",
 		"title"   => __( 'GSA Collection', 'flagship_sub_textdomain' ),
@@ -96,24 +96,24 @@ function flagship_sub_options_page_fields() {
 		"class"   => "nohtml",
 		"std"    => "krieger_collection");
 	$options[8] =
-	array (		
+	array (
 		"section" => "select_section",
 		"id"      => FLAGSHIP_SUB_SHORTNAME . "_calendar_address",
 		"title"   => __( 'Calendar URL', 'flagship_sub_textdomain' ),
 		"desc"    => __( 'Enter the URL of your Site Executive calendar instance', 'flagship_sub_textdomain' ),
 		"type"    => "text",
 		"class"   => "nohtml",
-		"std"    => "");		
+		"std"    => "");
 	$options[9] =
-	array (		
+	array (
 		"section" => "footer_section",
 		"id"      => FLAGSHIP_SUB_SHORTNAME . "_quicklinks",
 		"title"   => __( 'Quicklinks', 'flagship_sub_textdomain' ),
 		"desc"    => __( 'Do you want to use quicklinks from another site?', 'flagship_sub_textdomain' ),
 		"type"    => "checkbox",
-		"std"    => "1");		
+		"std"    => "1");
 	$options[10] =
-	array (		
+	array (
 		"section" => "footer_section",
 		"id"      => FLAGSHIP_SUB_SHORTNAME . "_quicklinks_id",
 		"title"   => __( 'Quicklinks Site ID', 'flagship_sub_textdomain' ),
@@ -122,7 +122,7 @@ function flagship_sub_options_page_fields() {
 		"class"   => "numeric",
 		"std"    => "1");
 	$options[11] =
-	array (		
+	array (
 		"section" => "footer_section",
 		"id"      => FLAGSHIP_SUB_SHORTNAME . "_copyright",
 		"title"   => __( 'Department Address', 'flagship_sub_textdomain' ),
@@ -130,23 +130,23 @@ function flagship_sub_options_page_fields() {
 		"type"    => "textarea",
 		"std"    => "Zanvyl Krieger School of Arts & Sciences");
 	$options[12] =
-	array (		
+	array (
 		"section" => "directory_section",
 		"id"      => FLAGSHIP_SUB_SHORTNAME . "_role_search",
 		"title"   => __( 'Filter by Role', 'flagship_sub_textdomain' ),
 		"desc"    => __( 'Do you want to be able to filter by role (faculty, research staff, emertiti)?', 'flagship_sub_textdomain' ),
 		"type"    => "checkbox",
-		"std"    => "0");		
+		"std"    => "0");
 	$options[13] =
-	array (		
+	array (
 		"section" => "directory_section",
 		"id"      => FLAGSHIP_SUB_SHORTNAME . "_research_search",
 		"title"   => __( 'Filter by Expertise', 'flagship_sub_textdomain' ),
 		"desc"    => __( 'Do you want to be able to filter by expertise/research area?', 'flagship_sub_textdomain' ),
 		"type"    => "checkbox",
-		"std"    => "0");	
+		"std"    => "0");
 	$options[14] =
-	array (		
+	array (
 		"section" => "homepage_section",
 		"id"      => FLAGSHIP_SUB_SHORTNAME . "_news_query_cond",
 		"title"   => __( 'News Feed Option', 'flagship_sub_textdomain' ),
@@ -154,7 +154,7 @@ function flagship_sub_options_page_fields() {
 		"type"    => "checkbox",
 		"std"    => "0");
 	$options[15] =
-	array (		
+	array (
 		"section" => "technical_section",
 		"id"      => FLAGSHIP_SUB_SHORTNAME . "_isis_name",
 		"title"   => __( 'ISIS Department Name', 'flagship_sub_textdomain' ),
@@ -163,7 +163,7 @@ function flagship_sub_options_page_fields() {
 		"class"   => "nohtml",
 		"std"    => "");
 	$options[16] =
-	array (		
+	array (
 		"section" => "select_section",
 		"id"      => FLAGSHIP_SUB_SHORTNAME . "_color_scheme",
 		"title"   => __( 'Color Scheme', 'flagship_sub_textdomain' ),
@@ -172,7 +172,7 @@ function flagship_sub_options_page_fields() {
 		"choices" => array('blue','black','yellow','green','purple','red','aqua'),
 		"std"    => "blue");
 	return $options;
-		
+
 }
 
 ?>
