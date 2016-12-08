0552c1b2bb88d89a4fe753e78f2cd823e04b3cf9
ksascomm <ksasweb@jhu.edu>
2016-12-08 13:26:45 -0500

    added ai1ec shortcode to front-page. added css & js for hp styles. removed full thumb from single-ai1ec posts.

--- front-page.php
+++ front-page.php
@@ -1,93 +1,79 @@
 <?php get_header(); ?>
 	<div id="content">
-		<?php $theme_option = flagship_sub_get_global_options(); ?>
-			
-	
+
 		<div id="inner-content" class="row">
 
 		    <div id="main" class="small-12 large-8 large-push-2 intro columns" role="main">
-				 
+
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 			    	<?php get_template_part( 'parts/loop', 'page' ); ?>
-			    
+
 			    <?php endwhile; endif; ?>
+
 			     <hr>
+
 			</div> <!-- end #main -->
 
 			<div class="small-12 large-8 columns">
-				
-			<?php //Events Query	 
-				 $events_query = new WP_Query(array(
-					'post_type' => 'post',
-					'orderby' => 'date',
-					'order' => 'asc',
-					'tax_query' => array(
-						array(
-							'taxonomy' => 'category',
-							'field' => 'slug',
-							'terms' => array( 'news' ),
-							'operator' => 'NOT IN'
-						)
-					)
-				)); 
-				if ( $events_query->have_posts() ) : ?>
+
 				<section class="events-feed">
-				   <h1>Events</h1>
-				   	 <?php while ($events_query->have_posts()) : $events_query->the_post(); ?>
+				   <h1>AGHI-sponsored Events</h1>
+
+					 	<?php echo do_shortcode('[ai1ec view="agenda" cat_name="AGHI" events_limit="4"]');	?>
 							
-					<?php get_template_part( 'parts/loop', 'events' ); ?>
-					<?php endwhile; ?>
-			
+						<br>
+
 					<div class="row padding-10">
 						<h2>
-							<a href="<?php echo site_url();?>/category/events/">View All Events</a>
+							<a href="<?php echo site_url();?>/events/cat_ids~75/">View All AGHI-sponsored Events</a>
 						</h2>
 					</div>
-				</section>
 
+				</section>
+				
 				<br>
 
-				<?php endif; ?>
-				
 				<section class="news-feed">
 				   <h1>News & Announcements</h1>
-				   		<?php  //News Query		
-								$news_query = new WP_Query(array(
-									'post_type' => 'post',
-									'tax_query' => array(
-										array(
-											'taxonomy' => 'category',
-											'field' => 'slug',
-											'terms' => array( 'events' ),
-											'operator' => 'NOT IN'
-										)
-									) 
-								)); 
-	
-	
-							if ( $news_query->have_posts() ) : while ($news_query->have_posts()) : $news_query->the_post(); ?>
-							
-								<?php get_template_part( 'parts/loop', 'news' ); ?>
-								
-							<?php endwhile; endif; ?>
+				   		<?php  //News Query
+				   		if ( false === ( $news_query = get_transient( 'news_mainpage_query' ) ) ) {
+							$news_query = new WP_Query(array(
+								'post_type' => 'post',
+								'tax_query' => array(
+									array(
+										'taxonomy' => 'category',
+										'field' => 'slug',
+										'terms' => array( 'events' ),
+										'operator' => 'NOT IN'
+									)
+								)
+							));
+						set_transient( 'news_mainpage_query', $news_query, 2592000 );
+						} 	
+
+						if ( $news_query->have_posts() ) : while ($news_query->have_posts()) : $news_query->the_post(); ?>
+
+							<?php get_template_part( 'parts/loop', 'news' ); ?>
+
+						<?php endwhile; endif; ?>
 					<div class="row padding-10">
 						<h2>
 							<a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>">News & Announcements Archive
 							</a>
 						</h2>
-					</div>			
+					</div>
 				</section>
 			</div>
 
-		 	<aside class="small-12 large-4 columns hide-for-print"> 
+		 	<aside class="small-12 large-4 columns hide-for-print" id="sidebar">
 
 				<?php if ( is_active_sidebar( 'sidebar1' ) ) { ?>
 					<?php get_sidebar(); ?>
 				<?php } ?>
 				<?php get_sidebar('homepage'); ?>
 
-			</aside>		
+			</aside>
 		</div> <!-- end #inner-content -->
 
 	</div> <!-- end #content -->
--- parts/loop-single.php
+++ parts/loop-single.php
@@ -1,14 +1,17 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-						
-	<header class="article-header">	
+
+	<header class="article-header">
 		<h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
     </header> <!-- end article header -->
-					
+
     <div class="entry-content" itemprop="articleBody">
-		<?php the_post_thumbnail('full'); ?>
+		<?php if (!is_singular('ai1ec_event')) : ?>
+			<?php the_post_thumbnail('full'); ?>
+		<?php endif;?>
 		<?php the_content(); ?>
+
 	</div> <!-- end article section -->
-						
-								
-													
+
+
+
 </article> <!-- end article -->
