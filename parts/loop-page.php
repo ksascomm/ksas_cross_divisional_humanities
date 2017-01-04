394ce885d156a307d68fd38a4551e6be6b959cbe
ksascomm <ksasweb@jhu.edu>
2017-01-04 12:29:14 -0500

    hide <header> if its front-page

--- parts/loop-page.php
+++ parts/loop-page.php
@@ -1,10 +1,11 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> itemscope itemtype="http://schema.org/WebPage">
 						
-	<header class="article-header">
+	
 	<?php if ( !is_front_page() ) : ?>
-		<h1 class="page-title"><?php the_title(); ?></h1>
+		<header class="article-header">
+			<h1 class="page-title"><?php the_title(); ?></h1>
+		</header> <!-- end article header -->	
 	<?php endif; ?>
-	</header> <!-- end article header -->
 					
     <div class="entry-content" itemprop="articleBody">
 	    <?php the_content(); ?>
