fd08017b0f9f7333a92beb29825158dc1798fbd9
ksascomm <ksasweb@jhu.edu>
2017-01-26 13:44:11 -0500

    schema markup

--- parts/loop-archive.php
+++ parts/loop-archive.php
@@ -1,10 +1,10 @@
-<article <?php post_class(''); ?> aria-labelledby="post-<?php the_ID(); ?>">				
+<article <?php post_class(''); ?> itemscope itemtype="http://schema.org/BlogPosting" aria-labelledby="post-<?php the_ID(); ?>">				
 	<div class="article-header">
-		<h2>
+		<h2 itemprop="headline">>
 			<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>" id="post-<?php the_ID(); ?>">
 				<?php the_title(); ?>
 			</a>
-		</h2>
+		</h2 >
 		<?php if (in_category( 'news')) : ?>
 			<h4><?php get_template_part( 'parts/content', 'byline' ); ?></h4>
 		<?php endif;?>
