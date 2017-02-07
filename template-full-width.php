cd87d3a85f81fe59040ecbf3887faef6c52e52a3
ksascomm <ksasweb@jhu.edu>
2017-02-07 14:32:40 -0500

    remove redundant ARIA roles

--- template-full-width.php
+++ template-full-width.php
@@ -10,7 +10,7 @@ Template Name: Full Width (No Sidebar)
 	
 		<div id="inner-content" class="row">
 	
-		    <main id="main" class="large-12 medium-12 columns" role="main">
+		    <main id="main" class="large-12 medium-12 columns">
 				
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
--- template-page-with-sidebar.php
+++ template-page-with-sidebar.php
@@ -10,7 +10,7 @@ Template Name: Page with Sidebar
 	
 		<div id="inner-content" class="row">
 	
-		    <main id="main" class="small-12 large-8 large-push-3 columns" role="main">
+		    <main id="main" class="small-12 large-8 large-push-3 columns">
 	
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
@@ -20,7 +20,7 @@ Template Name: Page with Sidebar
 				
 			</main> <!-- end #main -->
 		    
-		    <aside class="small-12 large-3 large-pull-9 columns hide-for-print" role="navigation" id="sidebar"> 
+		    <aside class="small-12 large-3 large-pull-9 columns hide-for-print" id="sidebar"> 
 				
 				<?php get_template_part( 'parts/nav', 'sidebar' ); ?>
 					
