9ae4020aaf690ece7bc5fe3fcf82b14af1087042
ksascomm <ksasweb@jhu.edu>
2017-03-30 14:24:25 -0400

    footer links & social media

--- footer.php
+++ footer.php
@@ -1,5 +1,32 @@
 					<footer class="footer hide-for-print">
 						<div id="inner-footer" class="row">
+
+							<!-- Footer Links -->
+							<div class="small-12 medium-9 columns" role="navigation" aria-labelledby="menu-footer-links">
+								<ul id="menu-footer-links" class="menu hide-for-small-only" role="menu">
+									<li role="menuitem"><a href="<?php echo get_site_url(); ?>/sitemap">Sitemap</a></li>
+								<?php if(get_page_by_title('Jobs') || get_page_by_title('Employment Opportunities') || get_page_by_title('Employment') ) : ?>
+									<li role="menuitem"><a href="<?php echo get_site_url(); ?>/about/jobs/">Employment</a></li>
+								<?php else : ?>
+									<li role="menuitem"><a href="http://krieger.jhu.edu/faculty-jobs/">Employment</a></li>	
+								<?php endif;?>
+									<li role="menuitem"><a href="http://krieger.jhu.edu/communications-office/">Communications Office</a></li>
+									<li role="menuitem"><a href="https://www.jhu.edu/alert/">Emergency Alerts & Info</a></li>
+								</ul>
+							</div>
+							<!-- Social Media -->
+							<div class="small-12 medium-3 large-2 columns end" role="navigation" aria-labelledby="social-media">
+								<div class="small-6 columns">
+									<a href="http://facebook.com/jhuksas" title="Facebook"><span class="fa fa-facebook-official fa-3x"></span><span class="screen-reader-text">Facebook</span></a>
+								</div>
+								<div class="small-6 columns">
+									<a href="https://www.youtube.com/user/jhuksas" title="YouTube"><span class="fa fa-youtube-square fa-3x"></span><span class="screen-reader-text">YouTube</span></a>
+								</div>
+							</div>
+
+
+
+
 						<!-- Copyright and Address -->
 								<div class="row" id="copyright" role="contentinfo">
 									<div class="small-12 columns">
