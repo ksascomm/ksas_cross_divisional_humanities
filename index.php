6587514837610f7664eaa54af7ba9bc96fa67703
ksascomm <ksasweb@jhu.edu>
2017-03-30 09:31:27 -0400

    remove highlights from news archive

--- index.php
+++ index.php
@@ -8,21 +8,31 @@
 		     	<?php the_breadcrumb(); ?>
 		    		<h1 class="page-title">News & Announcements <?php the_archive_title();?></h1>
 
-					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			 		
-						<!-- To see additional archive styles, visit the /parts directory -->
-						<?php get_template_part( 'parts/loop', 'archive' ); ?>
-			
-				<?php endwhile; ?>	
+					<?php $paged = (get_query_var('paged')) ? (int) get_query_var('paged') : 1;
+						if ( false === ( $news_archive_query = get_transient( 'news_archive_query_' . $paged ) ) ) {
+								$news_archive_query = new WP_Query(array(
+									'post_type' => 'post',
+									'tax_query' => array(
+										array(
+											'taxonomy' => 'category',
+											'field' => 'slug',
+											'terms' => array( 'news' ),
+										)
+									),
+									'posts_per_page' => 7,
+									'paged' => $paged)); 
+								set_transient( 'news_archive_query_' . $paged, $news_archive_query, 2592000 );
+							} 	
 
-					<?php joints_page_navi(); ?>
-					
-				<?php else : ?>
-											
-					<?php get_template_part( 'parts/content', 'missing' ); ?>
-						
-				<?php endif; ?>
-																								
+							while ($news_archive_query->have_posts()) : $news_archive_query->the_post(); ?>
+
+							<!-- To see additional archive styles, visit the /parts directory -->
+								<?php get_template_part( 'parts/loop', 'archive' ); ?>
+			
+							<?php endwhile; ?>	
+
+						<?php joints_page_navi(); ?>		
+																									
 		    </main> <!-- end #main -->
 		    
 			<div class="small-12 large-3 large-pull-9 columns hide-for-print archive"> 
