5b559fc9d465d345c223a53363ccc568a1b889e1
ksascomm <ksasweb@jhu.edu>
2017-08-17 15:24:11 -0400

    layout edits

--- template-fellows-directory.php
+++ template-fellows-directory.php
@@ -35,35 +35,19 @@
 										<?php the_post_thumbnail('directory', array('class' => 'padding-five floatleft hide-for-small-only')); ?>
 									<?php } ?>			    
 									<?php if ( get_post_meta($post->ID, 'ecpt_bio', true) || get_post_meta($post->ID, 'ecpt_thesis', true) ) { ?> 
-										<h4 class="no-margin"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+										<h3 class="no-margin"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 									<?php } else { ?>
-										<h4 class="no-margin"><?php the_title(); ?></h4>
+										<h3 class="no-margin"><?php the_title(); ?></h3>
 									<?php } ?>
-									<?php if ( get_post_meta($post->ID, 'ecpt_position', true) ) : ?><h5><?php echo get_post_meta($post->ID, 'ecpt_position', true); ?></h5><?php endif; ?>
-									<?php if ( get_post_meta($post->ID, 'ecpt_degrees', true) ) : ?><h5><?php echo get_post_meta($post->ID, 'ecpt_degrees', true); ?></h5><?php endif; ?>
+									<?php if ( get_post_meta($post->ID, 'ecpt_degrees', true) ) : ?><h4><?php echo get_post_meta($post->ID, 'ecpt_degrees', true); ?></h4><?php endif; ?>
 									<p class="contact no-margin">
-										<?php if ( get_post_meta($post->ID, 'ecpt_phone', true) ) : ?>
-											<span class="icon-mobile"><?php echo get_post_meta($post->ID, 'ecpt_phone', true); ?></span>
-										<?php endif; ?>
-										<?php if ( get_post_meta($post->ID, 'ecpt_fax', true) ) : ?>
-											<span class="icon-printer"><?php echo get_post_meta($post->ID, 'ecpt_fax', true); ?></span>
-										<?php endif; ?>
-										<?php if ( get_post_meta($post->ID, 'ecpt_email', true) ) : ?>
-											<span class="icon-mail"><a href="mailto:<?php echo get_post_meta($post->ID, 'ecpt_email', true); ?>"><?php echo get_post_meta($post->ID, 'ecpt_email', true); ?></a></span>
-										<?php endif; ?>
-										<?php if ( get_post_meta($post->ID, 'ecpt_office', true) ) : ?>
-											<span class="icon-location"><?php echo get_post_meta($post->ID, 'ecpt_office', true); ?></span>
-										<?php endif; ?>
-										<?php if ( get_post_meta($post->ID, 'ecpt_website', true) ) : ?>
-				    						<span class="icon-globe">
-												<a href="<?php echo get_post_meta($post->ID, 'ecpt_website', true); ?>" target="_blank">Personal Website</a>
-											</span>
-										<?php endif; ?>
+										<?php if ( get_post_meta($post->ID, 'ecpt_email', true) ) : ?><span class="fa fa-envelope" aria-hidden="true"></span> <a href="mailto:<?php echo get_post_meta($post->ID, 'ecpt_email', true); ?>"> <?php echo get_post_meta($post->ID, 'ecpt_email', true); ?></a><br><?php endif; ?>
 									</p>
 								<?php if ( get_post_meta($post->ID, 'ecpt_expertise', true) ) : ?>
 									<p><strong>Research Interests:&nbsp;</strong>
 										<?php echo get_post_meta($post->ID, 'ecpt_expertise', true); ?></p>
 								<?php endif; ?>
+
 								</div>	
 							</div>
 						</li>			
